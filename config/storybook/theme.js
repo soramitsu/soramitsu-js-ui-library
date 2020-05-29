cf153516e7ad17d19401c586a51a954e3cbc5e3b
Stefan Popov <stefanpopov@list.ru>
2020-05-29 11:03:55 +0400

    Configure storybook

---
+++ config/storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming/create'
+
+export default create({
+  base: 'light',
+
+  brandTitle: 'Soramitsu UI library',
+  brandUrl: 'https://soramitsu.co.jp/',
+  brandImage: 'https://static.tildacdn.com/tild3962-3961-4937-b235-363037623063/New_Soramitsu_Logo2.svg'
+})
