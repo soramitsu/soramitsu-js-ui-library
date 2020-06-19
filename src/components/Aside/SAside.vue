f3316a94bf083968fde65362530b4e94e0e63839
Stefan Popov <stefanpopov@list.ru>
2020-06-19 15:51:42 +0400

    implement aside component

---
+++ src/components/Aside/SAside.vue
@@ -0,0 +1,17 @@
+<template>
+  <s-aside></s-aside>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class SAside extends Vue {
+  @Prop({ default: '', type: String }) readonly type!: string
+}
+</script>
+
+<style lang="scss">
+@import "~@/styles/variables.scss";
+
+</style>
