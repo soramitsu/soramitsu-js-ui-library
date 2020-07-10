230ac409bbc875c34e89557a3b552d7bfe13b56f
Stefan Popov <stefanpopov@list.ru>
2020-07-10 07:48:21 +0400

    Add card story

---
+++ src/stories/SCard.stories.ts
@@ -0,0 +1,37 @@
+import { text, withKnobs, boolean, select, number } from '@storybook/addon-knobs'
+
+import { SCard, SRow, SButton } from '../components'
+import { CardShadow } from '../components/Card'
+
+export default {
+  component: SCard,
+  title: 'Design System/Card',
+  decorators: [withKnobs],
+  excludeStories: /.*Data$/
+}
+
+export const configurable = () => ({
+  components: { SCard, SRow, SButton },
+  template: `<s-row class="flex" style="flex: 1; justify-content: space-between; align-items: center;">
+               <s-card :shadow="shadow">
+                 <template slot="header">
+                   <span>{{ header }}</span>
+                   <s-button type="tertiary" size="medium">Close</s-button>
+                 </template>
+                 <div v-for="o in 4" :key="o" style="margin-bottom: 18px;">
+                   {{'List item ' + o }}
+                 </div>
+               </s-card>
+             </s-row>`,
+  data: () => ({
+    model: false
+  }),
+  props: {
+    shadow: {
+      default: select('Shadow', Object.values(CardShadow), CardShadow.HOVER)
+    },
+    header: {
+      default: text('Header', 'Card header')
+    }
+  }
+})
