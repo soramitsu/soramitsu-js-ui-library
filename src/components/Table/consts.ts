f5099f5fdfeaac1c7f0653981c85a4a04cb636ba
Stefan Popov <stefanpopov@list.ru>
2020-08-05 16:41:17 +0400

    Add types for table component

--- src/components/Table/consts.ts
+++ src/components/Table/consts.ts
@@ -1,3 +1,25 @@
 import { Size } from '@/types/size'
 
+export enum ColumnType {
+  SELECTION = 'selection',
+  INDEX = 'index',
+  EXPAND = 'expand'
+}
+
+export enum ColumnFixedPosition {
+  LEFT = 'left',
+  RIGHT = 'right'
+}
+
+export enum SortDirection {
+  ASC = 'ascending',
+  DESC = 'descending'
+}
+
+export enum ColumnAlignment {
+  LEFT = 'left',
+  CENTER = 'center',
+  RIGHT = 'right'
+}
+
 export const TableSize = Size
---
+++ src/components/Table/index.ts
@@ -0,0 +1,13 @@
+import STable from './STable.vue'
+import STableColumn from './STableColumn.vue'
+import { ColumnAlignment, ColumnFixedPosition, ColumnType, SortDirection, TableSize } from './consts'
+
+export {
+  STable,
+  STableColumn,
+  ColumnAlignment,
+  ColumnFixedPosition,
+  ColumnType,
+  SortDirection,
+  TableSize
+}
