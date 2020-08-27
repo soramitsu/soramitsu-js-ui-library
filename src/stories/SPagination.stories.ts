c5bd4b946c5794fae4392f07a040972587c7cb78
Stefan Popov <stefanpopov@list.ru>
2020-08-27 18:05:58 +0400

    Add story for pagination component

--- src/stories/SPagination.stories.ts
+++ src/stories/SPagination.stories.ts
@@ -1,4 +1,4 @@
-import { withKnobs, select } from '@storybook/addon-knobs'
+import { withKnobs, number, text, boolean } from '@storybook/addon-knobs'
 
 import { SPagination, SRow } from '../components'
 
@@ -12,9 +12,47 @@ export const configurable = () => ({
   components: { SRow, SPagination },
   template: `<s-row class="flex" style="flex: 1; flex-direction: column;">
                <s-pagination
+                 :total="total"
+                 :layout="layout"
+                 :disabled="disabled"
+                 :small="small"
+                 :background="background"
+                 :prev-text="prevText"
+                 :next-text="nextText"
+                 @size-change="handleSizeChange"
+                 @current-change="handleCurrentChange"
+                 @prev-click="handlePrevClick"
+                 @next-click="handleNextClick"
                >
                </s-pagination>
              </s-row>`,
   props: {
+    layout: {
+      default: text('Layout', 'total, sizes, pager, prev, next, jumper')
+    },
+    total: {
+      default: number('Total', 400)
+    },
+    disabled: {
+      default: boolean('Disabled', false)
+    },
+    small: {
+      default: boolean('Small', false)
+    },
+    background: {
+      default: boolean('Background', false)
+    },
+    prevText: {
+      default: text('Prev text', '')
+    },
+    nextText: {
+      default: text('Next text', '')
+    }
+  },
+  methods: {
+    handleSizeChange: (newSize: number) => console.log(`@size-change=${newSize}`),
+    handleCurrentChange: (newCurrent: number) => console.log(`@current-change=${newCurrent}`),
+    handlePrevClick: (newCurrent: number) => console.log(`@prev-click=${newCurrent}`),
+    handleNextClick: (newCurrent: number) => console.log(`@next-click=${newCurrent}`)
   }
 })
