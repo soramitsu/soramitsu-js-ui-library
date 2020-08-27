c3e3db1ab94dc56aa3bcedf6cad3c72788604e4e
Stefan Popov <stefanpopov@list.ru>
2020-08-27 13:36:48 +0400

    Add i18n

--- src/plugins/elementUI.ts
+++ src/plugins/elementUI.ts
@@ -1,107 +1,22 @@
 import Vue from 'vue'
-import {
-  Aside,
-  Badge,
-  Breadcrumb,
-  BreadcrumbItem,
-  Button,
-  ButtonGroup,
-  Card,
-  Checkbox,
-  Collapse,
-  CollapseItem,
-  Container,
-  DatePicker,
-  Dialog,
-  Divider,
-  Dropdown,
-  DropdownItem,
-  DropdownMenu,
-  Footer,
-  Form,
-  FormItem,
-  Header,
-  Input,
-  InputNumber,
+import Element, {
   Loading,
-  Main,
-  Menu,
-  MenuItem,
-  MenuItemGroup,
   Message,
   MessageBox as MsgBox,
-  Notification,
-  Option,
-  Pagination,
-  Radio,
-  RadioGroup,
-  Select,
-  Submenu,
-  Switch,
-  Table,
-  TableColumn,
-  TabPane,
-  Tabs,
-  Tag,
-  Tooltip,
-  Upload
+  Notification
 } from 'element-ui'
-// import lang from 'element-ui/lib/locale/lang/en'
-// import locale from 'element-ui/lib/locale'
 
-Vue.use(Breadcrumb)
-Vue.use(BreadcrumbItem)
-Vue.use(Collapse)
-Vue.use(CollapseItem)
-Vue.use(Dialog)
-Vue.use(Divider)
-Vue.use(Footer)
-Vue.use(Menu)
-Vue.use(MenuItem)
-Vue.use(MenuItemGroup)
-Vue.use(Submenu)
-Vue.use(Input)
-Vue.use(InputNumber)
-Vue.use(Radio)
-Vue.use(RadioGroup)
-Vue.use(Select)
-Vue.use(Option)
-Vue.use(Button)
-Vue.use(ButtonGroup)
-Vue.use(Table)
-Vue.use(TableColumn)
-Vue.use(DatePicker)
-Vue.use(Form)
-Vue.use(FormItem)
-Vue.use(Tag)
-Vue.use(Upload)
-Vue.use(Card)
-Vue.use(Container)
-Vue.use(Header)
-Vue.use(Aside)
-Vue.use(Main)
-Vue.use(Dropdown)
-Vue.use(DropdownMenu)
-Vue.use(DropdownItem)
-Vue.use(Tooltip)
-Vue.use(Switch)
-Vue.use(Badge)
-Vue.use(Pagination)
-Vue.use(Tabs)
-Vue.use(TabPane)
+import i18n from '../lang'
+
 Vue.use(Loading.directive)
-Vue.use(Checkbox)
+Vue.use(Element, { i18n: (key, value) => i18n.t(key, value) })
 const MessageBox = MsgBox
-MsgBox.setDefaults({
-  cancelButtonText: 'Cancel',
-  confirmButtonText: 'OK'
-})
 Vue.prototype.$loading = Loading.service
 Vue.prototype.$prompt = MessageBox.prompt
 Vue.prototype.$alert = MessageBox.alert
 Vue.prototype.$message = Message
 Vue.prototype.$notify = Notification
-// locale.use(lang) // TODO: it will be used later
+
 export {
   Loading,
   MessageBox,
