08a07fba9f251155f34277b96649a1432370c065
Stefan Popov <stefanpopov@list.ru>
2021-01-06 21:10:47 +0400

    Fix icons (#113)

--- src/components/Icon/SIcon.vue
+++ src/components/Icon/SIcon.vue
@@ -25,16 +25,18 @@ export default class SIcon extends Vue {
    */
   @Prop({ type: String, required: true }) readonly name!: string
 
+  private visible = false
+
+  mounted (): void {
+    this.checkVisibility()
+  }
+
   get computedClass (): string {
     if (!this.name) {
       return ''
     }
     if (this.name.startsWith('el-icon')) {
       return this.name
-    }
-    // Check loading state of icons font
-    if (!((document || {}) as any).fonts.check('1em soramitsu-icons')) {
-      return ''
     }
     if (!(Object.values(Icons) as Array<string>).includes(this.name)) {
       console.warn(`'${this.name}' was not found`)
@@ -48,7 +50,37 @@ export default class SIcon extends Vue {
     if (this.size) {
       styles.fontSize = !isNaN(+this.size) ? `${this.size}px` : this.size
     }
+    if (!this.visible) {
+      styles.color = 'transparent'
+    }
     return styles
+  }
+
+  private async checkVisibility (): Promise<void> {
+    const cssClass = this.computedClass
+    if (!cssClass) {
+      this.visible = false
+      return
+    }
+    const fonts = (document as any).fonts
+    if (!fonts) {
+      // Ignore this check if the browser has no support
+      this.visible = true
+      return
+    }
+    if (cssClass.startsWith('el-icon')) {
+      if (fonts.check('1em element-icons')) {
+        this.visible = true
+        return
+      }
+    } else {
+      if (fonts.check('1em soramitsu-icons')) {
+        this.visible = true
+        return
+      }
+    }
+    await new Promise(resolve => setTimeout(resolve, 50))
+    await this.checkVisibility()
   }
 }
 </script>
