845a2bd12489d36e7474f4a1ea74db5c871e8890
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2021-02-25 10:48:20 +0300

    use variables in offsets

--- src/stories/Select/SSelect.stories.ts
+++ src/stories/Select/SSelect.stories.ts
@@ -39,6 +39,7 @@ export const configurable = () => ({
                    :popper-class="borderRadius"
                    @change="handleChange"
                  >
+                   <div slot="prefix">123</div>
                    <s-option
                      v-for="option in options"
                      :key="option.value"
