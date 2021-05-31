c43704643bb6b5675cd75518cc8ded8b8876668e
Nikita Polyakov <53777036+Nikita-Polyakov@users.noreply.github.com>
2021-05-31 16:17:50 +0300

    Neumorphism/input (#206)
    * wip neumorphic input
    
    * add neu input styles & refactoring
    
    * fix file input

--- src/stories/SFloatInput.stories.ts
+++ src/stories/SFloatInput.stories.ts
@@ -1,6 +1,7 @@
-import { number, text, boolean, object, withKnobs } from '@storybook/addon-knobs'
+import { number, text, boolean, object, select, withKnobs } from '@storybook/addon-knobs'
 
-import { SFloatInput, SRow } from '../components'
+import { SFloatInput, SRow, SDesignSystemProvider } from '../components'
+import { DesignSystemTypes } from '../components/DesignSystem'
 
 export default {
   component: SFloatInput,
@@ -9,8 +10,10 @@ export default {
 }
 
 export const configurable = () => ({
-  components: { SFloatInput, SRow },
-  template: `<s-row class="s-flex" style="flex: 1; justify-content: space-between; align-items: center;">
+  components: { SFloatInput, SRow, SDesignSystemProvider },
+  template: `
+            <s-design-system-provider :value="designSystem">
+              <s-row class="s-flex" style="flex: 1; justify-content: space-between; align-items: center;">
                <s-float-input
                  v-model="model"
                  :decimals="decimals"
@@ -18,11 +21,15 @@ export const configurable = () => ({
                  :delimiters="delimiters"
                  :max="max"
                />
-             </s-row>`,
+              </s-row>
+            </s-design-system-provider>`,
   data: () => ({
     model: ''
   }),
   props: {
+    designSystem: {
+      default: select('Design System', Object.values(DesignSystemTypes), DesignSystemTypes.DEFAULT)
+    },
     decimals: {
       default: number('Decimals', 18)
     },
