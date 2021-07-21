9fb4714ea4a17f1873fc6e758bd15e1b4217c210
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2021-07-21 13:00:56 +0300

    fix storybook build scss

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -21,5 +21,14 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-knobs',
     '@storybook/addon-storysource'
-  ]
+  ],
+  webpackFinal: async (config) => {
+    const scssRule = config.module.rules.find(({ test }) => typeof test.test === 'function' && test.test('.scss'))
+    // important to include scss to storybook build
+    if (scssRule) {
+      scssRule.sideEffects = true
+    }
+
+    return config
+  }
 }
