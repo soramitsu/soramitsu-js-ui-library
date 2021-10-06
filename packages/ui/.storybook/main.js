8524fff42434ffede0ebb8936fa57ecb246ef428
Stefan Popov <stefanpopov@list.ru>
2021-10-07 02:19:18 +0400

    Remove examples & use vite as storybook builder

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -9,18 +9,12 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials'
   ],
-  webpackFinal: (config) => {
-    const scssRule = config.module.rules.find(({ test }) => typeof test.test === 'function' && test.test('.scss'))
-    // important to include scss to storybook build
-    if (scssRule) {
-      scssRule.sideEffects = true
-    }
-
-    config.module.rules.push({
-      test: /\.vue$/,
-      loader: require.resolve('vue-loader'),
-      include: path.resolve(__dirname, '../'),
-    });
+  core: {
+    builder: 'storybook-builder-vite',
+  },
+  async viteFinal(config, { configType }) {
+    config.resolve.alias['@'] = path.resolve(__dirname, '../src')
+    config.resolve.alias['~'] = path.resolve(__dirname, '..')
 
     return config
   }
