5e0b217440ade5f72d29bf1ffdf4f817a9cab99b
0x009922 <a.marcius26@gmail.com>
2021-11-03 11:46:02 +0300

    feat: implement ds provider & test it

---
+++ packages/ui/src/components/SDesignSystemProvider/SDesignSystemProvider.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { DesignSystem, DesignSystemProviderAPI, DESIGN_SYSTEM_KEY } from '@/state/design-system'
+import { computed, readonly, provide } from 'vue'
+
+const props = withDefaults(
+  defineProps<{
+    value?: DesignSystem
+  }>(),
+  { value: DesignSystem.Default },
+)
+
+const api: DesignSystemProviderAPI = readonly({
+  current: computed(() => props.value),
+})
+
+provide(DESIGN_SYSTEM_KEY, api)
+</script>
+
+<template>
+  <slot />
+</template>
--- packages/ui/src/lib.ts
+++ packages/ui/src/lib.ts
@@ -1,14 +1,18 @@
 export * from './components/Button'
 export * from './components/Input'
+export * from './components/SDesignSystemProvider'
+export { DesignSystem, useDesignSystem } from './state/design-system'
 
 import { Plugin } from 'vue'
 import { SButton } from './components/Button'
 import { SJsonInput, Autocomplete } from './components/Input'
+import { SDesignSystemProvider } from './components/SDesignSystemProvider'
 
 export function plugin(): Plugin {
   return (app) => {
     app.component('SButton', SButton)
     app.component('SJsonInput', SJsonInput)
     app.component('Autocomplete', Autocomplete)
+    app.component('SDesignSystemProvider', SDesignSystemProvider)
   }
 }
