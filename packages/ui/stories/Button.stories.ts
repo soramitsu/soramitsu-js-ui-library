3b63866e464cb081d8d2402cc844f783d944f17b
Stepan <kvantoreps@gmail.com>
2022-01-18 14:27:04 +0300

    WDS-53 add button types story

--- packages/ui/stories/Button.stories.ts
+++ packages/ui/stories/Button.stories.ts
@@ -1,44 +1,25 @@
-import { Meta, Story } from '@storybook/vue3'
-import { Component, FunctionalComponent, h, Slot } from 'vue'
+import { defineMeta, defineStory } from './util'
 import { SButton } from '@/lib'
 
-const meta: Meta = {
+export default defineMeta({
   title: 'Example/Button',
-}
-
-export default meta
-
-interface Props {
-  type?: 'primary' | 'secondary'
-  rounded?: boolean
-}
-
-interface Args {
-  props?: Props
-  defaultSlot: string | (() => Slot)
-}
-
-const Template: Story<Args> = ({ props, defaultSlot }) => ({
-  setup() {
-    return () => h(SButton, props, { default: typeof defaultSlot === 'string' ? () => defaultSlot : defaultSlot })
-  },
+  argTypes: {
+    type: {
+      defaultValue: 'primary',
+      options: ['primary', 'secondary', 'outline', 'action'],
+      control: { type: 'inline-radio' }
+    },
+  }
 })
 
-export const Primary = Template.bind({})
-Primary.args = { defaultSlot: 'Primary' }
-
-export const Secondary = Template.bind({})
-Secondary.args = {
-  props: { type: 'secondary' },
-  defaultSlot: 'Secondary',
-}
-
-export const Rounded = Template.bind({})
-Rounded.args = {
-  props: { rounded: true },
-  defaultSlot: 'Rounded',
-}
-
-// export const Secondary = (): FunctionalComponent => () => h(SButton, { type: 'secondary' }, 'Secondary button')
-
-// export const Rounded = (): FunctionalComponent => () => h(SButton, { rounded: true }, 'Rounded button')
+export const Button = defineStory((args) =>  ({
+  components: {
+    SButton,
+  },
+  setup() {
+    return args
+  },
+  template: `
+    <SButton :type="type">{{type}}</SButton>
+  `
+}))
