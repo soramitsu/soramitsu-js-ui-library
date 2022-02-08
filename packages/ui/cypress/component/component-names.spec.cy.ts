e6e888e357df840e2b5ba8be5b727e5fa2347402
0x009922 <a.marcius26@gmail.com>
2022-02-08 09:37:59 +0300

    test: fix

--- packages/ui/cypress/component/component-names.spec.cy.ts
+++ packages/ui/cypress/component/component-names.spec.cy.ts
@@ -1,13 +1,13 @@
 import { COMPONENTS } from '@/components/all-components'
 import { getComponentName } from '@/util'
 
-for (const [name, component] of Object.entries(COMPONENTS)) {
-  it(`Component "${name}" is named as "${name}"`, { retries: 0 }, () => {
+for (const [nameExpected, component] of Object.entries(COMPONENTS)) {
+  it(`Component "${nameExpected}" is named as "${nameExpected}"`, { retries: 0 }, () => {
     const nameExtracted = getComponentName(component)
 
     cy.wrap({ name: nameExtracted })
       // trick to avoid error retries
       .its('name', { timeout: 0 })
-      .should('eq', nameExtracted)
+      .should('eq', nameExpected)
   })
 }
