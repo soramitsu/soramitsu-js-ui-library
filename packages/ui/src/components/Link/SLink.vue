8d8788287b440e9f9260a123aeb7c70bd4ac562b
Stepan <kvantoreps@gmail.com>
2022-02-28 08:58:30 +0300

    WDS-77 remove redundant computed

--- packages/ui/src/components/Link/SLink.vue
+++ packages/ui/src/components/Link/SLink.vue
@@ -1,59 +1,43 @@
 <script lang="ts">
-export default defineComponent({name: 'SLink'})
+export default defineComponent({ name: 'SLink' })
 </script>
 
 <script setup lang="ts">
-import { computed } from 'vue'
 import { usePropTypeFilter } from '@/composables/prop-type-filter'
 import { IconBasicExternalLink24, IconStatusInfo } from '../icons'
 import { LINK_ICON_POSITION_VALUES, LINK_TYPE_VALUES } from './consts'
 import { LinkIconPosition, LinkType } from './types'
 
 const props = withDefaults(
-    defineProps<{
-      type?: LinkType,
-      iconPosition?: LinkIconPosition,
-      icon?: boolean,
-      tag?: string | object
-    }>(),
-    {
-      type: 'link',
-      iconPosition: 'right',
-      icon: false,
-      tag: 'a'
-    },
+  defineProps<{
+    type?: LinkType
+    iconPosition?: LinkIconPosition
+    icon?: boolean
+    tag?: string | object
+  }>(),
+  {
+    type: 'link',
+    iconPosition: 'right',
+    icon: false,
+    tag: 'a',
+  },
 )
-
 
 const definitelyType = usePropTypeFilter(() => props.type, LINK_TYPE_VALUES, 'link')
 const definitelyIconPosition = usePropTypeFilter(() => props.iconPosition, LINK_ICON_POSITION_VALUES, 'right')
-const finalTag = computed(() => props.tag || 'a')
 </script>
 
 <template>
   <component
-    :is="finalTag"
-    :class="[
-      's-link',
-      `s-link_type_${type}`,
-      `s-link_icon-position_${iconPosition}`,
-      'sora-tpg-p3',
-    ]"
+    :is="tag"
+    :class="['s-link', `s-link_type_${type}`, `s-link_icon-position_${iconPosition}`, 'sora-tpg-p3']"
   >
     <span>
       <slot />
     </span>
     <template v-if="icon">
-      <IconBasicExternalLink24
-        v-if="type === 'link'"
-        class="s-link__icon"
-        data-testid="icon"
-      />
-      <IconStatusInfo
-        v-else
-        class="s-link__icon"
-        data-testid="icon"
-      />
+      <IconBasicExternalLink24 v-if="type === 'link'" class="s-link__icon" data-testid="icon" />
+      <IconStatusInfo v-else class="s-link__icon" data-testid="icon" />
     </template>
   </component>
 </template>
