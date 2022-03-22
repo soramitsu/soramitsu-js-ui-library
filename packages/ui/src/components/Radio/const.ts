df8a1848070e0e65a2df325f68c69185961f2d69
0x009922 <a.marcius26@gmail.com>
2022-03-22 11:20:10 +0300

    fix: update radio typography

--- packages/ui/src/components/Radio/const.ts
+++ packages/ui/src/components/Radio/const.ts
@@ -1,19 +1,16 @@
 import { RadioSize } from './types'
 
-/**
- * FIXME wait for design
- */
 export const TYPOGRAPHY: Record<RadioSize, { label: string; description: string }> = {
   xl: {
     label: 'sora-tpg-p3',
     description: 'sora-tpg-p4',
   },
   lg: {
-    label: 'sora-tpg-p3',
+    label: 'sora-tpg-p4',
     description: 'sora-tpg-p4',
   },
   md: {
-    label: 'sora-tpg-p3',
+    label: 'sora-tpg-p4',
     description: 'sora-tpg-p4',
   },
 }
