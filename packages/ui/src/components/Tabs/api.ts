be06497581a0047b0f67e6bf7f72746b80008d3d
Stefan Popov <stefanpopov@list.ru>
2022-04-20 00:10:12 +0200

    Fix lint issues

--- packages/ui/src/components/Tabs/api.ts
+++ packages/ui/src/components/Tabs/api.ts
@@ -9,10 +9,7 @@ export interface TabsPanelApi {
 
 export const TABS_PANEL_BACKGROUND_TYPES = ['primary', 'secondary', 'none'] as const
 
-export type TabsPanelBackgroundType = 
-  typeof TABS_PANEL_BACKGROUND_TYPES extends ReadonlyArray<infer T>
-    ? T
-    : never
+export type TabsPanelBackgroundType = typeof TABS_PANEL_BACKGROUND_TYPES extends ReadonlyArray<infer T> ? T : never
 
 export const TABS_PANEL_API_KEY: InjectionKey<TabsPanelApi> = Symbol('tabsPanel')
 
