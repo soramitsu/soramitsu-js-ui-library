2b73acea5eb7d9ed7b3d1870bfbf09a57bb0791d
Stefan Popov <stefanpopov@list.ru>
2022-04-20 01:31:12 +0200

    Fix lint issues

--- packages/ui/stories/ProgressBar.stories.ts
+++ packages/ui/stories/ProgressBar.stories.ts
@@ -14,24 +14,22 @@ interface Args {
   activeBarColor?: string
 }
 
-
 const configurableStory = (args: Args) => ({
   components: { SProgressBar },
   template: `<div class="w-1/4"><SProgressBar  :percent="percent"
       :lineHeight="lineHeight">
   </SProgressBar></div>`,
   setup() {
-    const {percent, lineHeight} = args;
-    return { percent, lineHeight};
-  }
+    const { percent, lineHeight } = args
+    return { percent, lineHeight }
+  },
 })
 
-export const configurable = configurableStory.bind({});
+export const configurable = configurableStory.bind({})
 configurable.args = {
   percent: 50,
-  lineHeight: 2
-  }
-
+  lineHeight: 2,
+}
 
 const demoStory = (args: Args) => ({
   components: { SProgressBar },
@@ -39,21 +37,23 @@ const demoStory = (args: Args) => ({
       :lineHeight="lineHeight">
   </SProgressBar></div>`,
   setup() {
-    const { lineHeight } = args;
-    let loadingStatus = ref(0);
-    const interval = setInterval(async ()=> {
-      loadingStatus.value++;
-      if (loadingStatus.value===100) {
-        await new Promise(r => {setTimeout(r, 1000)});
-        loadingStatus.value = 0;
+    const { lineHeight } = args
+    let loadingStatus = ref(0)
+    const interval = setInterval(async () => {
+      loadingStatus.value++
+      if (loadingStatus.value === 100) {
+        await new Promise((r) => {
+          setTimeout(r, 1000)
+        })
+        loadingStatus.value = 0
       }
     }, 10)
-    return { loadingStatus, lineHeight };
-  }
+    return { loadingStatus, lineHeight }
+  },
 })
 
-export const demo = demoStory.bind({});
+export const demo = demoStory.bind({})
 
 demo.args = {
-  lineHeight: 2
-  }
+  lineHeight: 2,
+}
