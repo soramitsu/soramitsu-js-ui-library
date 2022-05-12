140671330a75a251605d22fa65d4aadccd666fc5
Stepan <kvantoreps@gmail.com>
2022-05-12 16:12:15 +0300

    WDS-57 add menu item pure visual component

---
+++ packages/ui/src/components/Menu/SMenuItemBody.vue
@@ -0,0 +1,104 @@
+<script lang="ts">
+export default { name: 'SMenuItemView' }
+</script>
+
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    tag?: string
+    submenuItem?: boolean
+    active?: boolean
+    minified?: boolean
+  }>(),
+  {
+    tag: 'div',
+    submenuItem: false,
+    active: false,
+    minified: false,
+  },
+)
+
+const emit = defineEmits<(event: 'click', value: HTMLElement) => void>()
+
+function handleClick(event: HTMLElement) {
+  emit('click', event)
+}
+</script>
+
+<template>
+  <component
+    :is="tag"
+    class="s-menu-item-body flex items-center cursor-pointer pl-24px pr-16px"
+    :class="{
+      's-menu-item-body_submenu-item': submenuItem,
+      's-menu-item-body_active': active,
+    }"
+    @click="handleClick"
+  >
+    <div class="s-menu-item-body__prepend flex-shrink-0 flex items-center justify-end h-16px w-16px mr-8px">
+      <slot
+        v-if="!submenuItem"
+        name="icon"
+        :class="'s-menu-item-body__icon'"
+      />
+    </div>
+
+    <div v-show="!minified">
+      <slot />
+    </div>
+
+    <div
+      v-show="!minified"
+      class="ml-auto pl-8px"
+    >
+      <slot
+        name="append"
+        :class="'s-menu-item-body__append'"
+      />
+    </div>
+  </component>
+</template>
+
+<style lang="scss">
+@use '@/theme';
+
+.s-menu-item-body {
+  $root: &;
+  background-color: #2e2e36; // Base [night] / Background
+  color: #ffffff; // Base [night] / ContentPrimary
+  height: 54px;
+
+  &_submenu-item {
+    height: 50px;
+    background-color: #26262d; // Utility [night] / Surface
+  }
+
+  &_active,
+  &:hover {
+    background-color: #26262d; // Utility [night] / Surface
+    box-shadow: inset 5px 0 theme.token-as-var('sys.color.primary');
+  }
+
+  &_submenu-item#{$root}_active,
+  &_submenu-item:hover {
+    box-shadow: none;
+
+    #{$root}__prepend::before {
+      content: '';
+      background-color: theme.token-as-var('sys.color.primary');
+      width: 5px;
+      height: 5px;
+    }
+  }
+
+  &__icon {
+    width: 100%;
+    height: 100%;
+    fill: currentColor;
+  }
+
+  &__append {
+    fill: currentColor;
+  }
+}
+</style>
