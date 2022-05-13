ed52def4a382981dc21c56a912b764c870221873
Stepan <kvantoreps@gmail.com>
2022-05-13 10:58:21 +0300

    WDS-57 add footer, header and multiline items to menu story

--- packages/ui/stories/SMenu.stories.ts
+++ packages/ui/stories/SMenu.stories.ts
@@ -15,32 +15,64 @@ export const Configurable = defineStory((args: Record<string, unknown>) => ({
   },
   setup() {
     const collapsed = ref(false)
-
+    const selectedItem = ref('1')
     return {
       args,
       collapsed,
+      selectedItem,
     }
   },
   template: `
-    <button @click="collapsed = !collapsed">collapse</button>
-    <SMenu v-bind="args" default-active="10" :collapsed="collapsed">
-      <SMenuItem v-for="i in 5" :index="String(i)">
-        <template #icon="iconProps">
-          <IconStatusInfo :class="iconProps.class" />
+    <div class="flex">
+      <div class="w-100px ml-16px order-1">
+        Selected:
+        <input type="text" v-model="selectedItem" class="block mb-16px border border-black">
+        <button @click="collapsed = !collapsed" class="block mb-16px border border-black">collapse</button>
+      </div>
+  
+      <SMenu v-bind="args" v-model="selectedItem" :collapsed="collapsed">
+        <template #header>
+          <div class="sora-tpg-h2 px-14px py-16px text-center divide-y divide-current">
+            <div v-if="!collapsed">Header</div>
+            <div v-else>H</div>
+          </div>
         </template>
-        Option {{ String(i) }}
-      </SMenuItem>
 
-      <SSubmenu>
-        <template #title>Submenu</template>
-        <SMenuItem v-for="i in 5" :index="String(i) + 0">
-          Option {{ String(i) + 0 }}
+        <SMenuItem v-for="i in 5" :index="String(i)">
+          <template #icon="iconProps">
+            <IconStatusInfo :class="iconProps.class" />
+          </template>
+          Option {{ String(i) }}
         </SMenuItem>
-      </SSubmenu>
+  
+        <SSubmenu>
+          <template #title>Submenu</template>
+          <SMenuItem v-for="i in 5" :index="String(i) + 0">
+            Option {{ String(i) + 0 }}
+          </SMenuItem>
+        </SSubmenu>
+  
+        <SMenuItem index="01">
+          <template #icon="iconProps">
+            <IconStatusInfo :class="iconProps.class" />
+          </template>
+          Long long truncated text
+        </SMenuItem>
 
-      <SMenuItem v-for="i in 5" :index="0 + String(i)">
-        Option {{ 0 + String(i) }}
-      </SMenuItem>
-    </SMenu>
+        <SMenuItem index="02">
+          <template #icon="iconProps">
+            <IconStatusInfo :class="iconProps.class" />
+          </template>
+          Long long Text with<br>line break
+        </SMenuItem>
+
+        <template #footer>
+          <div class="px-30px whitespace-nowrap text-center divide-y divide-current" :class="{ 'invisible': collapsed }">
+            <div class="py-16px">Footer content</div>
+            <div class="py-16px">Footer content</div>
+          </div>
+        </template>
+      </SMenu>
+    </div>
   `,
 }))
