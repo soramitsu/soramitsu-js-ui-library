ae19bd14440be2008175587e64f3df92c9df60aa
0x009922 <a.marcius26@gmail.com>
2022-07-15 14:08:12 +0600

    [test]: cover disabled mode case

--- packages/ui/src/components/Select/composable.model.spec.ts
+++ packages/ui/src/components/Select/composable.model.spec.ts
@@ -66,4 +66,22 @@ describe('Auto-closing', () => {
 
     expect(onAutoClose).not.toBeCalled()
   })
+
+  test('when valud is selected/unselected in single mode, but auto-close is disabled, it is not called', () => {
+    const onAutoClose = vi.fn()
+    const modeling = useFactory(
+      {
+        model: ref(null),
+        singleModeAutoClose: ref(false),
+        onAutoClose,
+      },
+      'single',
+    )
+
+    modeling.select('thin')
+    modeling.select('regular')
+    modeling.unselect('thin')
+
+    expect(onAutoClose).not.toBeCalled()
+  })
 })
