4a0c97f8384d89778cd20193fd7338eaa0d5bb82
Nikita-Polyakov <polyakov@soramitsu.co.jp>
2022-11-16 17:54:20 +0300

    update stories 2

--- src/stories/SRadio.stories.ts
+++ src/stories/SRadio.stories.ts
@@ -1,22 +1,46 @@
-import { withKnobs, select, boolean } from '@storybook/addon-knobs'
+import { Meta, Story } from '@storybook/vue'
 
 import { SRadio, SRadioGroup, SRow, SCol, SDesignSystemProvider } from '../components'
 import { Size } from '../types'
 
-export default {
-  component: SRadio,
-  title: 'Design System/Components/Radio 🟣',
-  decorators: [withKnobs],
-  excludeStories: /.*Data$/
-}
-
-export const radioData = [
+const radioData = [
   { label: 'first', title: 'First' },
   { label: 'second', title: 'Second' }
 ]
 
-export const configurable = () => ({
+export default {
+  component: SRadio,
+  title: 'Design System/Components/Radio 🟣',
+  excludeStories: /.*Data$/,
+  argTypes: {
+    size: {
+      name: 'Size',
+      control: {
+        type: 'select',
+        options: Object.values(Size)
+      },
+      defaultValue: Size.MEDIUM
+    },
+    disabled: {
+      name: 'Disabled',
+      control: {
+        type: 'boolean'
+      },
+      defaultValue: false
+    },
+    border: {
+      name: 'Border',
+      control: {
+        type: 'boolean'
+      },
+      defaultValue: false
+    }
+  }
+} as Meta
+
+export const configurable: Story = (args, { argTypes }) => ({
   components: { SRadio, SRow, SCol, SDesignSystemProvider },
+  props: Object.keys(argTypes),
   template: `<s-row style="flex: 1;" :gutter="20">
                <s-col v-for="item in items" :key="item.label" :span="6">
                  <s-radio
@@ -38,21 +62,10 @@ export const configurable = () => ({
     vModelValue: 'first',
     changeValue: '',
     items: radioData
-  }),
-  props: {
-    size: {
-      default: select('Size', Object.values(Size), Size.MEDIUM)
-    },
-    disabled: {
-      default: boolean('Disabled', false)
-    },
-    border: {
-      default: boolean('Border', false)
-    }
-  }
+  })
 })
 
-export const disabled = () => ({
+export const disabled: Story = () => ({
   components: { SRadio, SRow },
   template: `<s-row class="s-flex" style="flex: 1; justify-content: space-between; align-items: center;">
               <s-radio
@@ -71,7 +84,7 @@ export const disabled = () => ({
   })
 })
 
-export const withBorders = () => ({
+export const withBorders: Story = () => ({
   components: { SRadio, SRow },
   template: `<s-row class="s-flex" style="flex: 1; justify-content: space-between; align-items: center;">
               <s-radio
@@ -90,8 +103,9 @@ export const withBorders = () => ({
   })
 })
 
-export const radioButtonGroup = () => ({
+export const radioButtonGroup: Story = (args, { argTypes }) => ({
   components: { SRadio, SRadioGroup, SRow },
+  props: Object.keys(argTypes),
   template: `<s-row class="s-flex" style="flex-wrap: wrap;">
               <s-radio-group v-model="model" :size="size" :disabled="disabled">
                 <s-radio
@@ -99,7 +113,7 @@ export const radioButtonGroup = () => ({
                   :key="item.label"
                   :label="item.label"
                   :size="size"
-                  :isRadioButton="isRadioButton"
+                  :is-radio-button="true"
                 >
                   {{ item.title }}
                 </s-radio>
@@ -111,16 +125,5 @@ export const radioButtonGroup = () => ({
   data: () => ({
     model: 'first',
     items: radioData
-  }),
-  props: {
-    size: {
-      default: select('Size', Object.values(Size), Size.MEDIUM)
-    },
-    disabled: {
-      default: boolean('Disabled', false)
-    },
-    isRadioButton: {
-      default: boolean('Is radio buttons', true)
-    }
-  }
+  })
 })
--- src/stories/SSlider.stories.ts
+++ src/stories/SSlider.stories.ts
@@ -1,16 +1,125 @@
-import { text, number, boolean, select, object, withKnobs } from '@storybook/addon-knobs'
+import { Meta, Story } from '@storybook/vue'
 
 import { SSlider, SliderInputSize } from '../components/Slider'
 
 export default {
   component: SSlider,
   title: 'Design System/Components/Slider',
-  decorators: [withKnobs],
-  excludeStories: /.*Data$/
-}
+  excludeStories: /.*Data$/,
+  argTypes: {
+    min: {
+      name: 'Minimum value',
+      control: {
+        type: 'number',
+        min: 0
+      },
+      defaultValue: 0
+    },
+    max: {
+      name: 'Maximum value',
+      control: {
+        type: 'number',
+        min: 0
+      },
+      defaultValue: 100
+    },
+    disabled: {
+      name: 'Disabled',
+      control: {
+        type: 'boolean'
+      },
+      defaultValue: false
+    },
+    step: {
+      name: 'Step size',
+      control: {
+        type: 'number',
+        min: 1
+      },
+      defaultValue: 1
+    },
+    label: {
+      name: 'Label',
+      control: {
+        type: 'text'
+      }
+    },
+    showTooltip: {
+      name: 'Show Tooltip',
+      control: {
+        type: 'boolean'
+      },
+      defaultValue: true
+    },
+    showStops: {
+      name: 'Show Stops',
+      control: {
+        type: 'boolean'
+      },
+      defaultValue: false
+    },
+    showInput: {
+      name: 'Show Input',
+      control: {
+        type: 'boolean'
+      },
+      defaultValue: false
+    },
+    showInputControls: {
+      name: 'Show Input Controls',
+      control: {
+        type: 'boolean'
+      },
+      if: { arg: 'showInput' },
+      defaultValue: false
+    },
+    inputSize: {
+      name: 'Size of the input box',
+      control: {
+        type: 'select',
+        options: Object.values(SliderInputSize)
+      },
+      if: { arg: 'showInput' },
+      defaultValue: SliderInputSize.SMALL
+    },
+    debounce: {
+      name: 'Debounce delay when typing (in milliseconds)',
+      control: {
+        type: 'number',
+        min: 0
+      }
+    },
+    range: {
+      name: 'Range',
+      control: {
+        type: 'boolean'
+      },
+      defaultValue: false
+    },
+    marks: {
+      name: 'Range Marks',
+      control: {
+        type: 'object'
+      },
+      if: { arg: 'range' },
+      defaultValue: {
+        0: '0°C',
+        8: '8°C',
+        37: '37°C'
+      }
+    },
+    height: {
+      name: 'Height',
+      control: {
+        type: 'text'
+      }
+    }
+  }
+} as Meta
 
-export const configurable = () => ({
+const Template: Story = (args, { argTypes }) => ({
   components: { SSlider },
+  props: Object.keys(argTypes),
   template: `<div class="s-flex" style="flex: 1; flex-direction: column;">
               <s-slider
                 v-model="modelValue"
@@ -20,146 +129,13 @@ export const configurable = () => ({
                 :step="step"
                 :show-stops="showStops"
                 :disabled="disabled"
-                @change="(value) => changeValue = value"
-              />
-              <span style="margin-top: 20px;">
-                v-model="{{ modelValue }}", @change="{{ changeValue }}"
-              </span>
-            </div>`,
-  data: () => ({
-    modelValue: 0,
-    changeValue: 0
-  }),
-  props: {
-    min: {
-      default: number('Minimum value', 0)
-    },
-    max: {
-      default: number('Maximum value', 100)
-    },
-    showTooltip: {
-      default: boolean('Show Tooltip', true)
-    },
-    step: {
-      default: number('Step size', 1)
-    },
-    showStops: {
-      default: boolean('Show Stops', false)
-    },
-    disabled: {
-      default: boolean('Disabled', false)
-    }
-  }
-})
-
-export const withCustomLabelAttribute = () => ({
-  components: { SSlider },
-  template: `<div class="s-flex" style="flex: 1; flex-direction: column;">
-              <s-slider
-                v-model="modelValue"
                 :label="label"
-                @change="(value) => changeValue = value"
-              />
-              <span style="margin-top: 20px;">
-                v-model="{{ modelValue }}", @change="{{ changeValue }}", label="{{ label }}"
-              </span>
-            </div>`,
-  data: () => ({
-    modelValue: 0,
-    changeValue: 0,
-    label: 'Custom slider label value'
-  })
-})
-
-export const withInitValue = () => ({
-  components: { SSlider },
-  template: `<div class="s-flex" style="flex: 1; flex-direction: column;">
-              <s-slider
-                v-model="modelValue"
-                @change="(value) => changeValue = value"
-              />
-              <span style="margin-top: 20px;">
-                v-model="{{ modelValue }}", @change="{{ changeValue }}"
-              </span>
-            </div>`,
-  data: () => ({
-    modelValue: 50,
-    changeValue: 0
-  })
-})
-
-export const withInput = () => ({
-  components: { SSlider },
-  template: `<div class="s-flex" style="flex: 1; flex-direction: column;">
-              <s-slider
-                v-model="modelValue"
                 :show-input="showInput"
                 :show-input-controls="showInputControls"
                 :input-size="inputSize"
                 :debounce="debounce"
-                @change="(value) => changeValue = value"
-              />
-              <span style="margin-top: 20px;">
-                v-model="{{ modelValue }}", @change="{{ changeValue }}"
-              </span>
-            </div>`,
-  data: () => ({
-    modelValue: 0,
-    changeValue: 0
-  }),
-  props: {
-    showInput: {
-      default: boolean('Show Input', true)
-    },
-    showInputControls: {
-      default: boolean('Show Input Controls', true)
-    },
-    inputSize: {
-      default: select('Size of the input box', Object.values(SliderInputSize), SliderInputSize.SMALL)
-    },
-    debounce: {
-      default: number('Debounce delay when typing (in milliseconds)', 300)
-    }
-  }
-})
-
-export const withRange = () => ({
-  components: { SSlider },
-  template: `<div class="s-flex" style="flex: 1; flex-direction: column;">
-              <s-slider
-                v-model="modelValue"
-                :range="range"
-                :marks="range && marks"
-                @change="(value) => changeValue = value"
-              />
-              <span style="margin-top: 20px;">
-                v-model="{{ modelValue }}", @change="{{ changeValue }}"
-              </span>
-            </div>`,
-  data: () => ({
-    modelValue: [0, 0],
-    changeValue: [0, 0]
-  }),
-  props: {
-    range: {
-      default: boolean('Range', true)
-    },
-    marks: {
-      default: object('Marks', {
-        0: '0°C',
-        8: '8°C',
-        37: '37°C'
-      })
-    }
-  }
-})
-
-export const verticalMode = () => ({
-  components: { SSlider },
-  template: `<div class="s-flex" style="flex: 1; flex-direction: column;">
-              <s-slider
-                v-model="modelValue"
                 :height="height"
+                :range="range"
                 @change="(value) => changeValue = value"
               />
               <span style="margin-top: 20px;">
@@ -167,12 +143,31 @@ export const verticalMode = () => ({
               </span>
             </div>`,
   data: () => ({
-    modelValue: 0,
-    changeValue: 0
-  }),
-  props: {
-    height: {
-      default: text('Height', '200px')
-    }
-  }
+    modelValue: args.range ? [0, 0] : 0,
+    changeValue: args.range ? [0, 0] : 0
+  })
 })
+
+export const Configurable = Template.bind({})
+
+export const WithCustomLabelAttribute = Template.bind({})
+WithCustomLabelAttribute.args = {
+  label: 'Custom Label'
+}
+
+export const WithInput = Template.bind({})
+WithCustomLabelAttribute.args = {
+  showInput: true,
+  showInputControls: true,
+  debounce: 300
+}
+
+export const VerticalMode = Template.bind({})
+VerticalMode.args = {
+  height: '200px'
+}
+
+export const WithRange = Template.bind({})
+WithRange.args = {
+  range: true
+}
