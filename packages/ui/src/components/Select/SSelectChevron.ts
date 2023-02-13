44be61431d7bd08a2ceb3fe90b7f5fe7bcc99cee
Stepan <kvantoreps@gmail.com>
2023-02-13 11:50:20 +0300

    WDS-125 use right icons in select chevron and add switch between 16/24 variants

--- packages/ui/src/components/Select/SSelectChevron.ts
+++ packages/ui/src/components/Select/SSelectChevron.ts
@@ -1,22 +1,28 @@
-import { FunctionalComponent, h } from 'vue'
-import IconChevronBottom from '~icons/mdi/chevron-down'
+import { FunctionalComponent, h, PropType } from 'vue'
+import { IconChevronBottom16, IconArrowsChevronBottom24 } from '@/components/icons'
 import './SSelectChevron.scss'
 
 interface Props {
-  rotate?: boolean
+  rotate?: boolean,
+  variant?: 16 | 24
 }
 
-const component: FunctionalComponent<Props> = ({ rotate }) =>
-  h(IconChevronBottom, {
+const component: FunctionalComponent<Props> = ({ rotate, variant }) =>
+  h(variant === 24 ? IconArrowsChevronBottom24 : IconChevronBottom16, {
     class: [
       's-select-chevron',
       {
         's-select-chevron_rotate': rotate,
       },
     ],
+    width: '1em',
+    height: '1em',
   })
 
-component.props = { rotate: Boolean }
+component.props = {
+  rotate: Boolean,
+  variant: Number as PropType<16 | 24>
+}
 component.displayName = 'SSelectChevron'
 
 export default component
