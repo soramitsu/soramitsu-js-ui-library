634c473248499e431b7604f6d42689db32196e8d
Dmitry Balashov <a.marcius26@gmail.com>
2023-03-14 12:59:56 +0700

    [chore]: update configs

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,7 +1,12 @@
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
-  // TODO use other stories as well
-  stories: ['../stories/**/Alert.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    {
+      directory: '../stories/components',
+      titlePrefix: 'Components',
+      files: '**/*.stories.@(js|jsx|ts|tsx)',
+    },
+  ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/vue3-vite',
